7218b8b7a921b43070c92d8a5d14cc8da625de4d
Qui <thanhquy117@gmail.com>
2023-06-07 21:04:37 +0700

    Code mới

--- controller/main.js
+++ controller/main.js
@@ -1,12 +1,42 @@
-function layDanhSacSanPham() {
+function layDanhSachSanPham() {
     axios({
       method: 'get',
       url: 'https://shop.cyberlearn.vn/api/Product',
     }).then(function (result) {
-      console.log(result.data);
-      hienThiTable(result.data)
+      var data = result.data.content
+      hienThiTable(data)
     }).catch(function (error) {
       console.log(error)
     });
-  }
-  layDanhSacSanPham()
+}
+layDanhSachSanPham() 
+function hienThiTable(data){
+  var listSP = document.getElementById("shoe-product-Q");
+  listSP.innerHTML= '';
+  data.forEach(function (product) {
+    var productHTML = `
+    <div class="col-4 mt-4">
+      <div class="shoe-product">
+        <div class="shoe-card">
+          <div id="product-img">
+            <a href="#">
+              <img class ="shoes-img" src="${product.image}" alt="">
+            </a>
+          </div>
+          <div class="product-infor">
+            <h5>${product.name}</h5>
+            
+          </div>
+          <div class="shoe-price">
+            <p>$${product.price}</p>
+      
+            <button class="btn shoebtn-button">Thông Tin</button>
+            <button class="btn shoebtn-button">Mua ngay <span class="fa-solid fa-cart-shopping"></span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    listSP.innerHTML += productHTML
+  })
+}
---
+++ controller/sanpham.js
@@ -0,0 +1,8 @@
+function SanPham(ma,ten,giaSP,mieuTa,hinhAnh,soLuong) {
+    this.id = ma;
+    this.name = ten;
+    this.price =giaSP;
+    this.description = mieuTa;
+    this.image = hinhAnh;
+    this.quantity = soLuong;
+}
